4efb31f7da93a05ed3a04dc5e10a0bf8972f1e50
Tofigkh1 <khaniyefdevelop@gmail.com>
2024-01-28 11:46:04 +0400

    removedark mode

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -28,28 +28,5 @@ overlaymodal.onclick = function(){
 };
 
 
-let isDarkMode = false;
 
-function toggleDarkMode() {
-    isDarkMode = !isDarkMode;
-    const body = document.body;
-    const personInfo = document.getElementById('personInfo');
-    const container1 = document.querySelector('.container1');
-    const container2 = document.querySelector('.container2');
 
-    
-
-    if (isDarkMode) {
-        body.classList.add('dark-mode');
-        setTimeout(()=>{
-            personInfo.classList.add('dark-mode');
-            container1.classList.add('dark-mode');
-            container2.classList.add('dark-mode');
-        }, 10)
-    } else {
-        body.classList.remove('dark-mode');
-        personInfo.classList.remove('dark-mode');
-        container1.classList.remove('dark-mode');
-        container2.classList.remove('dark-mode');
-    }
-}
