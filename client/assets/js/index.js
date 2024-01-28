7ed76b6096a5482398904162c25cdae21e388ca0
Tofigkh1 <khaniyefdevelop@gmail.com>
2024-01-28 09:19:01 +0400

    dark mode

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -26,3 +26,30 @@ overlaymodal.onclick = function(){
         
     }, 300)
 };
+
+
+let isDarkMode = false;
+
+function toggleDarkMode() {
+    isDarkMode = !isDarkMode;
+    const body = document.body;
+    const personInfo = document.getElementById('personInfo');
+    const container1 = document.querySelector('.container1');
+    const container2 = document.querySelector('.container2');
+
+    
+
+    if (isDarkMode) {
+        body.classList.add('dark-mode');
+        setTimeout(()=>{
+            personInfo.classList.add('dark-mode');
+            container1.classList.add('dark-mode');
+            container2.classList.add('dark-mode');
+        }, 10)
+    } else {
+        body.classList.remove('dark-mode');
+        personInfo.classList.remove('dark-mode');
+        container1.classList.remove('dark-mode');
+        container2.classList.remove('dark-mode');
+    }
+}
